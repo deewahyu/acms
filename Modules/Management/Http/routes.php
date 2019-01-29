4fe113bf4620796c2ce0f3b216e75f9100ba746c
Didin Wahyudin <deewahyu@Didins-MacBook-Pro.local>
2019-01-29 23:27:11 +0700

    make it better

--- Modules/Management/Http/routes.php
+++ Modules/Management/Http/routes.php
@@ -10,7 +10,7 @@ Route::group(['middleware' => 'web', 'prefix' => 'management', 'namespace' => 'M
     Route::get('/telegram-update', 'TelegramController@updatedActivity');
     Route::get('/send-message', 'TelegramController@sendMessage');
     Route::post('/store-message', 'TelegramController@storeMessage');
-    Route::post('/'. '786415118:AAGVh5ixRGHkTQfx0fnV0X5yJhW_t4pCJZg' . '/webhook', 'TelegramController@handleRequest');
+    Route::post('/786415118:AAGVh5ixRGHkTQfx0fnV0X5yJhW_t4pCJZg', 'TelegramController@handleRequest');
     Route::get('/set-hook', 'TelegramController@setWebHook');
     Route::get('/remove-hook', 'TelegramController@removeWebHook');
     Route::get('/getme', 'TelegramController@getMe');
